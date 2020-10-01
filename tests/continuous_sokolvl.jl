d8dd72b151da68701c309226d933bdfd9ec5865b
HervierLucas <lucas.hervier@student.isae-supaero.fr>
2020-10-01 12:28:52 +0200

    Add test on save and load functions

--- tests/continuous_sokolvl.jl
+++ tests/continuous_sokolvl.jl
@@ -34,12 +34,22 @@ mutate(i::ContinuousSokoLvl) = mutate(i, cfg.m_rate)
     # test the map string
     expected_str = """
     bbbb
-    bbbb
+    bAbb
     bbbb
     bbbb
     """
-    lvl_str = write_map(ind)
+    lvl_str = write_map!(ind)
     @test lvl_str == expected_str
+    @test ind.output_map[1] == expected_str
     new_ind = mutate(ind)
     @test new_ind.genes != genes
+    ind.fitness[1] = 0
+    save_ind(ind,"tests//save_continuous.dna")
+    load_str = read("tests//save_continuous.dna", String)
+    load_ind = ContinuousSokoLvl(load_str,model)
+    @test load_ind.genes == ind.genes
+    @test load_ind.width == ind.width
+    @test load_ind.height == ind.height
+    @test load_ind.objects_char_list == ind.objects_char_list
+    @test load_ind.output_map == ind.output_map
 end
