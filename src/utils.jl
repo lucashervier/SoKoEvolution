de1639ab24cb18651e783a057231444aad4cf911
HervierLucas <lucas.hervier@student.isae-supaero.fr>
2020-10-07 16:58:07 +0200

    Add function to use our Soko Individual into a GAEvo or sNES (Cambrian)

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,6 @@
 using Flux
+using EvolutionaryStrategies
+import EvolutionaryStrategies: snes_populate, snes_generation, sNESState
 
 function get_params_count(model)
     size = 0
@@ -27,4 +29,90 @@ function load_weights_from_array!(model,weights)
         end
         layer_idx +=1
     end
+end
+
+#-------------Utils to create GA of SokoAgent or ContinuousSokoLvl-------------#
+function initialize(itype::Type, model, cfg::NamedTuple)
+    population = Array{itype}(undef, cfg.n_population)
+    for i in 1:cfg.n_population
+        population[i] = itype(model,cfg)
+    end
+    population
+end
+
+function GAEvo{T}(model, cfg::NamedTuple, fitness::Function;
+                      logfile=string("logs/", cfg.id, ".csv")) where T
+    logger = CambrianLogger(logfile)
+    population = initialize(T, model, cfg)
+    GAEvo(cfg, logger, population, fitness, 0)
+end
+
+#------------Utils to create sNES of SokoAgent or ContinuousSokoLvl------------#
+mutable struct sNES{T} <: Cambrian.AbstractEvolution
+    config::NamedTuple
+    logger::CambrianLogger
+    population::Array{T}
+    elites::Array{T}
+    state::sNESState
+    fitness::Function
+    gen::Int
+end
+
+function snes_init(itype::Type, model, cfg::NamedTuple, state::EvolutionaryStrategies.ESState)
+    population = Array{itype}(undef, cfg.n_population)
+    for i in 1:cfg.n_population
+        genes = state.μ .+ state.σ .* view(state.s, :, i)
+        population[i] = itype(genes,model,cfg)
+    end
+    population
+end
+
+function sNES{T}(model, cfg::NamedTuple, fitness::Function, state::EvolutionaryStrategies.sNESState;
+     logfile=string("logs/", cfg.id, ".csv")) where T
+    logger = CambrianLogger(logfile)
+    population = snes_init(T, model, cfg, state)
+    elites = deepcopy([population[i] for i in 1:cfg.n_elite])
+    sNES{T}(cfg, logger, population, elites, state, fitness, 0)
+end
+
+function sNES{T}(model, cfg::NamedTuple, fitness::Function;
+    logfile=string("logs/", cfg.id, ".csv")) where T
+    logger = CambrianLogger(logfile)
+    cfg = merge(EvolutionaryStrategies.snes_config(cfg.n_genes), cfg)
+    state = sNESState(cfg.n_genes, cfg.n_population)
+    sNES{T}(model, cfg, fitness, state; logfile=logfile)
+end
+
+function snes_populate(e::sNES{T}) where T
+    for i in eachindex(e.population)
+        e.population[i].genes .= e.state.μ .+ e.state.σ .* view(e.state.s, :, i)
+        e.population[i].fitness .= -Inf
+    end
+end
+
+function snes_generation(e::sNES{T}) where T
+    d = e.config.n_genes
+    n = e.config.n_population
+
+    # copy population information
+    F = zeros(n)
+    for i in eachindex(e.population)
+        F[i] = -e.population[i].fitness[1]
+    end
+    idx = sortperm(F)
+
+    # compute gradients
+    ∇μ = zeros(d)
+    ∇σ = zeros(d)
+    for i in 1:n
+        j = idx[i]
+        ∇μ .+= e.state.u[i] .* e.state.s[:, j]
+        ∇σ .+= e.state.u[i] .* (e.state.s[:, j].^2 .- 1.0)
+    end
+
+    # update state variables
+    e.state.μ .+= e.config.ημ .* e.state.σ .* ∇μ
+    e.state.σ .*= exp.(e.config.ησ/2 .* ∇σ)
+    randn!(e.state.s)
+    Cambrian.elites_generation(e)
 end
