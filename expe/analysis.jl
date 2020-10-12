96f718fcfffb49dae8fa0780be31a1ab6d78fa8d
HervierLucas <lucas.hervier@student.isae-supaero.fr>
2020-10-12 17:01:04 +0200

    Disable rendering while replaying agents

--- expe/analysis.jl
+++ expe/analysis.jl
@@ -49,8 +49,8 @@ gdy_path = joinpath(@__DIR__,"..","resources","games")
 gdy_reader = Griddly.GDYReader(image_path,shader_path)
 
 grid = Griddly.load!(gdy_reader,joinpath(gdy_path,"Single-Player/GVGAI/$game_name"))
-game = Griddly.create_game(grid,Griddly.SPRITE_2D)
-player1 = Griddly.register_player!(game,"Tux", Griddly.BLOCK_2D)
+game = Griddly.create_game(grid,Griddly.VECTOR)
+player1 = Griddly.register_player!(game,"Tux", Griddly.VECTOR)
 Griddly.init!(game)
 #-------------------------------Fitness Analysis-------------------------------#
 fitness_mean_envs = env_mat_logs[:,6]
@@ -62,51 +62,49 @@ fitness_std_agents = agent_mat_logs[:,7]
 title_plot = "Fitness of a CoEvolution between Sokoban level and Sokoban players"
 function plot_results(title_plot,gen_start,gen_step,gen_end,figure_saving_path)
     xs = [i for i in gen_start:gen_step:gen_end]
-    plot(xs,fitness_mean_envs[Int(gen_start/gen_step):Int(gen_end/gen_step)],ribbon=fitness_std_envs[Int(gen_start/gen_step):Int(gen_end/gen_step)],fillalpha=.5,label="Envs fitness",background=:lightgray,xaxis="Generation number",yaxis="Fitness",title=title_plot, size=(700,700));
+    plot(xs,fitness_mean_envs[Int(gen_start/gen_step):Int(gen_end/gen_step)],ribbon=fitness_std_envs[Int(gen_start/gen_step):Int(gen_end/gen_step)],fillalpha=.5,label="Envs fitness",xaxis="Generation number",yaxis="Fitness",title=title_plot, size=(700,700));
     plot!(xs,fitness_mean_agents[Int(gen_start/gen_step):Int(gen_end/gen_step)],ribbon=fitness_std_agents[Int(gen_start/gen_step):Int(gen_end/gen_step)],fillalpha=.5,label="Agents fitness");
     savefig(figure_saving_path)
 end
-# #---------------------Best Fitness Agent Overall-------------------------------#
-# best_agent_gen = agent_mat_logs[argmax(agent_mat_logs[:,5]),4]
-# best_agent_path = Formatting.format("$agents_gen_path//{1:04d}//0013.dna",best_agent_gen)
-# best_agent_overall_string = read("$best_agent_path", String)
-# best_agent = SokoAgent(best_agent_overall_string,agent_model)
-#
-# rendering = false
-#
-# # replay the best agents on its own envs
-# best_agent_envs_path = Formatting.format("$envs_gen_path//{1:04d}",best_agent_gen)
-# # if your envs are SokoLvlIndividual
-# best_own_rewards = replay_sokolvl(best_agent,best_agent_envs_path;render=rendering)
-# # # if your envs are ContinuousSokoLvl
-# # best_own_rewards = replay_continuoussokolvl(best_agent,best_agent_envs_path,envs_model;render=rendering)
-#
-# # comparison with the last agent to see if its get better generall results
-# last_gen_agent_path = Formatting.format("$agents_gen_path//{1:04d}//0013.dna",gen_end)
-# last_gen_agent_str = read(last_gen_agent_str,String)
-# last_gen_agent = SokoAgent(last_gen_agent_str,agent_model)
-#
-# # if your envs are SokoLvlIndividual
-# last_bestenvs_rewards = replay_sokolvl(last_gen_agent,best_agent_envs_path;render=rendering)
-# # # if your envs are ContinuousSokoLvl
-# # last_bestenvs_rewards = replay_continuoussokolvl(last_gen_agent,best_agent_envs_path,envs_model;render=rendering)
-#
-# last_gen_envs_path = Formatting.format("$envs_gen_path//{1:04d}",gen_end)
-#
-# # if your envs are SokoLvlIndividual
-# last_own_rewards = replay_sokolvl(last_gen_agent,last_gen_envs_path;render=rendering)
-# # # if your envs are ContinuousSokoLvl
-# # last_own_rewards = replay_continuoussokolvl(last_gen_agent,last_gen_envs_path,envs_model;render=rendering)
-#
-# # if your envs are SokoLvlIndividual
-# best_lastenvs_rewards = replay_sokolvl(best_agent,last_gen_envs_path;render=rendering)
-# # # if your envs are ContinuousSokoLvl
-# # best_lastenvs_rewards = replay_continuoussokolvl(best_agent,last_gen_envs_path,envs_model;render=rendering)
-#
-# println("best_agent_gen:$best_agent_gen")
-# println("best agent on its own envs:\n$best_own_rewards")
-# println("last agent on the best agent envs:\n$last_bestenvs_rewards")
-# println("best agent on last envs:\n$best_lastenvs_rewards")
-# println("last agent on its own envs:\n$last_own_rewards")
+#---------------------Best Fitness Agent Overall-------------------------------#
+best_agent_gen = agent_mat_logs[argmax(agent_mat_logs[:,5]),4]
+best_agent_path = Formatting.format("$agents_gen_path//{1:04d}//0013.dna",best_agent_gen)
+best_agent_overall_string = read("$best_agent_path", String)
+best_agent = SokoAgent(best_agent_overall_string,agent_model)
+
+# replay the best agents on its own envs
+best_agent_envs_path = Formatting.format("$envs_gen_path//{1:04d}",best_agent_gen)
+# if your envs are SokoLvlIndividual
+best_own_rewards = replay_sokolvl(best_agent,best_agent_envs_path)
+# # if your envs are ContinuousSokoLvl
+# best_own_rewards = replay_continuoussokolvl(best_agent,best_agent_envs_path,envs_model)
+
+# comparison with the last agent to see if its get better generall results
+last_gen_agent_path = Formatting.format("$agents_gen_path//{1:04d}//0013.dna",gen_end)
+last_gen_agent_str = read(last_gen_agent_str,String)
+last_gen_agent = SokoAgent(last_gen_agent_str,agent_model)
+
+# if your envs are SokoLvlIndividual
+last_bestenvs_rewards = replay_sokolvl(last_gen_agent,best_agent_envs_path)
+# # if your envs are ContinuousSokoLvl
+# last_bestenvs_rewards = replay_continuoussokolvl(last_gen_agent,best_agent_envs_path,envs_model)
+
+last_gen_envs_path = Formatting.format("$envs_gen_path//{1:04d}",gen_end)
+
+# if your envs are SokoLvlIndividual
+last_own_rewards = replay_sokolvl(last_gen_agent,last_gen_envs_path)
+# # if your envs are ContinuousSokoLvl
+# last_own_rewards = replay_continuoussokolvl(last_gen_agent,last_gen_envs_path,envs_model)
+
+# if your envs are SokoLvlIndividual
+best_lastenvs_rewards = replay_sokolvl(best_agent,last_gen_envs_path)
+# # if your envs are ContinuousSokoLvl
+# best_lastenvs_rewards = replay_continuoussokolvl(best_agent,last_gen_envs_path,envs_model)
+
+println("best agent generation:$best_agent_gen")
+println("best agent on its own envs:\n$best_own_rewards")
+println("last agent on the best agent envs:\n$last_bestenvs_rewards")
+println("best agent on last envs:\n$best_lastenvs_rewards")
+println("last agent on its own envs:\n$last_own_rewards")
 #--------------------------------Main------------------------------------------#
 plot_results(title_plot,gen_start,gen_step,gen_end,figure_saving_path)
